4530d9c6b2250d161af3a061de669357af7c6ae3
hackanonimous <esi.segurity@gmail.com>
2022-04-19 12:28:47 -0500

    Creacion de un ventana modal con less

---
+++ script.js
@@ -0,0 +1,5 @@
+let mostrarModal=document.querySelector('#showModal')
+mostrarModal.onclick= ()=>{document.querySelector(".modal").classList.add("active")}
+
+let ocultarModal=document.querySelector('.modal')
+ocultarModal.onclick=()=>{document.querySelector(".modal").classList.remove("active")}
